12d596ac9001aaad089991ecbc7b85456f558fef
P3D Service <info.p3dservice@yandex.ru>
2026-09-19 09:58:41 +0300

    fix: pin bootstrap default to v0.6.0

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -eu
 
 REPO="P3DService/P3D-K1-Deployment"
-REF="${P3D_K1_REF:-v0.5.1}"
+REF="${P3D_K1_REF:-v0.6.0}"
 RAW_BASE="https://raw.githubusercontent.com/${REPO}/${REF}"
 DEST="/usr/data/scripts/p3d-k1"
 TMP="/tmp/p3d-k1-bootstrap.$$"
